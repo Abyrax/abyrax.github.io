fc47d983b587394a19d665da7f492bfc704fd65b
Ahmet Burak Yuksel <abyrax@gmail.com>
2026-04-20 19:17:51 +0300

    upd

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,16 +19,7 @@ jQuery(document).ready(function($) {
 		l.addEventListener('click', () => { new bootstrap.Collapse(menuToggle).toggle() })
 	})
 
-   if ($('.ds-testimonials-section').length) {
-		$('.ds-testimonials-slider').slick({
-		  	infinite: true,
-		  	arrows: true,
-		  	autoplay: true,
-		  	autoplaySpeed: 4000,
-		  	prevArrow:"<button type='button' class='slick-prev slick-arrow'><i class='ri-arrow-left-line'></i></button>",
-		  	nextArrow:"<button type='button' class='slick-next slick-arrow'><i class='ri-arrow-right-line'></i></button>"
-		});
-    }
+
    
 
 });
